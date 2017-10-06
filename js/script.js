b52ec92e78dfa5dd232547515f64998e619d195d
ElenaShkurko <shkurko.lena2012@gmail.com>
2017-10-06 18:20:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+;(function($){
+
+	$(document).ready(function(){
+
+	$('.ba-slider').slick({
+
+		'dots': false,
+		'prevArrow': '<a href="#" class="ba-slider__button ba-slider__button--prev">Prev</a>',
+  		'nextArrow': '<a href="#" class="ba-slider__button ba-slider__button--next">Next</a>'
+		});
+
+	});
+
+})(jQuery);
